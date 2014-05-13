a4044dbd93790c4914c93db59ab122817026a197
Thibault Clerice <thibault@algorythme.net>
2014-05-13 20:43:46 +0100

    Commented code with ajax functionnality

--- modules/portal/app/controllers/portal/Guides.scala
+++ modules/portal/app/controllers/portal/Guides.scala
@@ -53,7 +53,16 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     EntityType.Country)
   private val defaultSearchParams = SearchParams(entities = defaultSearchTypes, sort = Some(SearchOrder.Score))
 
+/*
+*
+*
+*   General functionnalities for SQL DATA retrieval
+*
+*/
 
+  /*
+  *  Return a menu
+  */
   def menu(id: Int):List[GuidesPage] = DB.withConnection { implicit connection =>
     SQL(
       """
@@ -75,61 +84,10 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     }.toList
   }
 
-  def listGuides() = userProfileAction { implicit userOpt => implicit request => 
-
-    val guides: List[GuidesData] = DB.withConnection { implicit connection =>
-      SQL(
-        """
-          SELECT * FROM research_guide
-        """
-      ).apply().map { row =>
-        GuidesData(
-          row[Option[Int]]("id_research_guide"),
-          row[String]("name_research_guide"),
-          row[String]("path_research_guide"),
-          row[Option[String]]("picture_research_guide"),
-          row[Option[String]]("description_research_guide")
-        )
-      }.toList
-    }
-    Ok(p.guides.guidesList(guides))
-  }
-
-  /* Guides */
-  def home(guide: String) = {
-    guideLayout(guide, "places")
-  }
-
-  def layoutRetrieval(guide:String, key: String) = { 
-    guideLayout(guide, key)
-  }
-
-  def guide(guideId: Int): GuidesData = DB.withConnection { implicit connection =>
-    SQL(
-      """
-        SELECT 
-          * 
-        FROM 
-          research_guide
-        WHERE 
-          id_research_guide = {id} 
-        LIMIT 1
-      """
-    ).on('id -> guideId).apply().headOption.map { row =>
-      GuidesData(
-        row[Option[Int]]("id_research_guide"),
-        row[String]("name_research_guide"),
-        row[String]("path_research_guide"),
-        row[Option[String]]("picture_research_guide"),
-        row[Option[String]]("description_research_guide")
-      )
-    }.head
-  }
-
-  def guideLayout(guide:String, key: String) = {
-
-    val template: Option[GuidesPage] = DB.withConnection { implicit connection =>
-
+  /*
+  *  Return a template
+  */
+  def template(guide:String, key:String): Option[GuidesPage] = DB.withConnection { implicit connection =>
       SQL(
         """
           SELECT 
@@ -162,8 +120,99 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
       }
     }
 
+  /*
+  *  Return a guide given its ID
+  */
+  def guide(guideId: Int): GuidesData = DB.withConnection { implicit connection =>
+    SQL(
+      """
+        SELECT 
+          * 
+        FROM 
+          research_guide
+        WHERE 
+          id_research_guide = {id} 
+        LIMIT 1
+      """
+    ).on('id -> guideId).apply().headOption.map { row =>
+      GuidesData(
+        row[Option[Int]]("id_research_guide"),
+        row[String]("name_research_guide"),
+        row[String]("path_research_guide"),
+        row[Option[String]]("picture_research_guide"),
+        row[Option[String]]("description_research_guide")
+      )
+    }.head
+  }
+
+
+  /*
+  *  Return SearchParams for items with hierarchy
+  */
+  def getParams(request: Request[Any], eT: EntityType.Value ): Option[SearchParams] = {
+    request.getQueryString("parent") match {
+      case Some(parent) => Some(SearchParams(query = Some("parentId:" + parent), entities = List(eT)))
+      case _ => Some(SearchParams(query = Some("isTopLevel:true"), entities = List(eT)))
+    }
+  }
+
+/*
+*
+*   Routes functions for normal HTML
+*
+*/
+
+  /*
+  * Return a list of guides
+  */
+def listGuides() = userProfileAction { implicit userOpt => implicit request => 
+
+    val guides: List[GuidesData] = DB.withConnection { implicit connection =>
+      SQL(
+        """
+          SELECT * FROM research_guide
+        """
+      ).apply().map { row =>
+        GuidesData(
+          row[Option[Int]]("id_research_guide"),
+          row[String]("name_research_guide"),
+          row[String]("path_research_guide"),
+          row[Option[String]]("picture_research_guide"),
+          row[Option[String]]("description_research_guide")
+        )
+      }.toList
+    }
+    Ok(p.guides.guidesList(guides))
+  }
+
+  /*
+  * Return a homepage for a guide
+  */
+  def home(guide: String) = {
+    guideLayout(template(guide, "places"))
+  }
+
+  /*
+  * Return a layout for a guide and a given path
+  */
+  def layoutRetrieval(guide:String, key: String) = { 
+    guideLayout(template(guide, key))
+  }
+
+
+
+/*
+*
+* Link a layout [GuidesPage] to a correct template function
+*
+*/
+
+
+
+
+  def guideLayout(temp: Option[GuidesPage]) = {
     /* Function mapping */
-    template match {
+    temp match {
       case Some(t) if t.layout == "person" => guideAuthority(t, Map("holderId" -> t.content))
       case Some(t) if t.layout == "map" => guideMap(t, Map("holderId" -> t.content))
       case Some(t) if t.layout == "keyword" => guideKeyword(t, Map("holderId" -> t.content))
@@ -175,13 +224,17 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     }
   }
 
-  def getParams(request: Request[Any], eT: EntityType.Value ): Option[SearchParams] = {
-    request.getQueryString("parent") match {
-      case Some(parent) => Some(SearchParams(query = Some("parentId:" + parent), entities = List(eT)))
-      case _ => Some(SearchParams(query = Some("isTopLevel:true"), entities = List(eT)))
-    }
-  }
 
+
+/*
+*
+*   Layouts function, add a new function for a new type of layout
+*
+*/
+
+  /*
+  *   Layout named "person" [HistoricalAgent]
+  */
   def guideAuthority(template: GuidesPage, params: Map[String, String]) = userBrowseAction.async { implicit userDetails => implicit request =>
     searchAction[HistoricalAgent](params, defaultParams = Some(SearchParams(entities=List(EntityType.HistoricalAgent)))
       ) {
@@ -191,6 +244,9 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     }.apply(request)
   }
 
+  /*
+  *   Layout named "keyword" [Concept]
+  */
   def guideKeyword(template: GuidesPage, params: Map[String, String]) = userBrowseAction.async { implicit userDetails => implicit request =>
     searchAction[Concept](params, defaultParams = Some(SearchParams(entities = List(EntityType.Concept))),
       entityFacets = conceptFacets) {
@@ -199,6 +255,9 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     }.apply(request)
   }
 
+  /*
+  *   Layout named "map" [Concept]
+  */
   def guideMap(template: GuidesPage, params: Map[String, String]) = userBrowseAction.async { implicit userDetails => implicit request =>
     searchAction[Concept](params, defaultParams = Some(SearchParams(entities = List(EntityType.Concept))),
       entityFacets = conceptFacets) {
@@ -207,6 +266,9 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     }.apply(request)
   }
 
+  /*
+  *   Layout named "organisation" [Concept]
+  */
   def guideOrganization(template: GuidesPage, params: Map[String, String]) = userBrowseAction.async { implicit userDetails => implicit request =>
     searchAction[Concept](params, defaultParams = getParams(request, EntityType.Concept),
       entityFacets = conceptFacets
@@ -216,8 +278,70 @@ case class Guides @Inject()(implicit globalConfig: global.GlobalConfig, searchDi
     }.apply(request)
   }
 
+  /*
+  *   Layout named "md" [Markdown]
+  */
   def guideMarkdown(template: GuidesPage, content: String) = userProfileAction { implicit userOpt => implicit request =>
     Ok(p.guides.markdown(template -> (guide(template.parent) -> menu(template.parent)), content))
+  }
+
+
+/*
+*
+* Ajax functionnalities for guides
+*
+* Can be tested through
+    $.post(
+    "http://localhost:9000/guides/:guide/:page",
+    {},
+    function(data) {},
+    "html")
+*
+*
+*/
+
+  /*
+  *   Route function, retrieve a layout for given identifiers
+  */
+  def layoutAjaxRetrieval(guide:String, key:String) = {
+    /* Function mapping */
+    template(guide, key) match {
+      case Some(t) => guideAjax(t, Map("holderId" -> t.content))
+      case _ => Action { implicit request =>
+        NotFound(views.html.errors.pageNotFound())
+      }
+    }
+  }
+
+  /*
+  *
+  *   Research items and print them given a correct layout
+  *
+  */
+  def guideAjax(template: GuidesPage, params: Map[String, String]) = {
+    template match {
+      case t if template.layout == "keyword" || template.layout == "organisation" || template.layout == "places" => userBrowseAction.async { implicit userOpt => implicit request =>
+        searchAction[Concept](params, defaultParams = getParams(request, EntityType.Concept),
+          entityFacets = conceptFacets
+        ) {
+            page => params => facets => _ => _ =>
+              Ok(p.guides.ajax(template -> (guide(template.parent) -> menu(template.parent)), page, params))
+        }.apply(request)
+      }
+      case t if template.layout == "person" => userBrowseAction.async { implicit userDetails => implicit request =>
+        searchAction[HistoricalAgent](params, defaultParams = Some(SearchParams(entities=List(EntityType.HistoricalAgent)))
+          ) {
+            page => params => facets => _ => _ =>
+          Ok(p.guides.ajax(template -> (guide(template.parent) -> menu(template.parent)), page, params))
+
+        }.apply(request)
+      }
+      case _ => Action { implicit request =>
+        NotFound(views.html.errors.pageNotFound())
+      }
+
+    }
+    
   }
 
 }
